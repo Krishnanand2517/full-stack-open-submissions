cfb98cc13c477694bd78b0dd1cec10ab8efcb70a
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-10-01 19:21:17 +0530

    Done exercise 6a

---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,65 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'VOTE': {
+      const anecdoteId = action.payload.id;
+      const anecdoteToChange = state.find(anecdote => anecdote.id === anecdoteId);
+  
+      const updatedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      };
+  
+      const newState = state.map(a => a.id === anecdoteId ? updatedAnecdote : a);
+      
+      return newState;
+    }
+    case 'CREATE': {
+      return state.concat(action.payload);
+    }
+    default:
+      return state;
+  }
+};
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'CREATE',
+    payload: {
+      content,
+      id: generateId(),
+      votes: 0
+    }
+  };
+};
+
+export const voteAction = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  };
+};
+
+export default reducer
